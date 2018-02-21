cea8cc53ab6c8384cd6d5c07463379ea42ec06b4
Andrino Van Der Beek <j_andrinovanderbeek@tss-hr114-dm2.local>
2018-02-21 13:16:11 -0500

    Normal changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 // JavaScript Document
 
-console.log("Linked up");
+
+function() {
+	"use strict";
+	console.log("Linked up");
+	
+	
+	document.getElementsById("Image1").addEventListener("mouseover", mouseOver);
+	document.getElementsById("Image1").addEventListener("mouseout", mouseOut);
+	
+	function mouseOver() {
+		document.getElementsById("Image1").scale.outerWidth = "25px";
+	}) ();
+	
+	function mouseOut() {
+		document.getElementsById("Image1").scale.innerWidth = "35px";
+	}) ();
+	
+}) ();
